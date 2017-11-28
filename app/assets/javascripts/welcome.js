3157a28ebdab88cc8b0eb9bfb18a6859394a4c19
maryna25 <kalenskaya.maryna@gmail.com>
2017-11-29 01:07:16 +0200

    Add more tasks

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,16 +1,65 @@
 $(document).ready(function(){
     $("#btn1").click(function(){
-        $.ajax({
+      request = $.ajax({
           type: "POST",
           url: "/welcome/first_task",
-          data: { price: $("#input1").val() }
-          });
+          data: { price: $("#input1").val() },
+          success: function(result){
+            $("#div1").text() = result;
+          }});
     });
     $("#btn2").click(function(){
         $.ajax({
           type: "POST",
           url: "/welcome/second_task",
-          data: { genre: $("#input2").val() }
-          });
+          data: { genre: $("#input2").val() },
+          success: function(result){
+            $("#div2").html(result);
+          }});
+    });
+    $("#btn3").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/welcome/third_task",
+          data: { genre: $("#input3").val() },
+          success: function(result){
+            $("#div3").html(result);
+          }});
+    });
+    $("#btn4").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/welcome/task4",
+          data: { genre: $("#input4").val() },
+          success: function(result){
+            $("#div4").html(result);
+          }});
+    });
+    $("#btn5").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/welcome/task5",
+          data: { price: $("#input5").val() },
+          success: function(result){
+            $("#div5").html(result);
+          }});
+    });
+    $("#btn6").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/welcome/task6",
+          data: { name: $("#input6").val() },
+          success: function(result){
+            $("#div6").html(result);
+          }});
+    });
+    $("#btn7").click(function(){
+        $.ajax({
+          type: "POST",
+          url: "/welcome/task7",
+          data: { name: $("#input7").val() },
+          success: function(result){
+            $("#div7").html(result);
+          }});
     });
 });
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,6 +13,7 @@ class WelcomeController < ApplicationController
     @answer1 = ''
     records_array.each { |arr| arr.each { |a| @answer1 += a + ' ' } }
     p @answer1
+    render json: @answer1, status: :ok
   end
 
   def second_task
@@ -24,7 +25,75 @@ class WelcomeController < ApplicationController
            WHERE books.genre = '#{params[:genre]}'))"
     records_array = ActiveRecord::Base.connection.execute(sql).to_a
     @answer2 = ''
-      records_array.each { |arr| arr.each { |a| @answer2 += a + ' ' } }
+    records_array.each { |arr| arr.each { |a| @answer2 += a + ' ' } }
     p @answer2
+  end
+
+  def third_task
+    sql = "SELECT authors.name
+           FROM authors
+           WHERE(authors.id in
+           (SELECT books.author_id
+           FROM books
+           WHERE books.genre != '#{params[:genre]}'))"
+    records_array = ActiveRecord::Base.connection.execute(sql).to_a
+    @answer3 = ''
+    records_array.each { |arr| arr.each { |a| @answer3 += a + ' ' } }
+    p @answer3
+  end
+
+  def task4
+    sql = "SELECT authors.name
+           FROM authors
+           WHERE(authors.id not in
+           (SELECT books.author_id
+           FROM books
+           WHERE books.genre = '#{params[:genre]}'))"
+    records_array = ActiveRecord::Base.connection.execute(sql).to_a
+    @answer4 = ''
+    records_array.each { |arr| arr.each { |a| @answer4 += a + ' ' } }
+    p @answer4
+  end
+
+  def task5
+    sql = "SELECT users.name
+           FROM users
+           WHERE(users.id not in
+           (SELECT orders.user_id
+           FROM orders
+           WHERE orders.price = #{params[:price]}))"
+    records_array = ActiveRecord::Base.connection.execute(sql).to_a
+    @answer5 = ''
+    records_array.each { |arr| arr.each { |a| @answer5 += a + ' ' } }
+    p @answer5
+  end
+
+  def task6
+    sql = "SELECT librarians.name
+           FROM librarians
+           WHERE(librarians.id in
+           (SELECT orders.librarian_id
+           FROM orders INNER JOIN users on orders.user_id = users.id
+           WHERE users.name = '#{params[:name]}'
+           GROUP BY orders.librarian_id
+           HAVING COUNT(orders.user_id) >= 2))"
+    records_array = ActiveRecord::Base.connection.execute(sql).to_a
+    @answer6 = ''
+    records_array.each { |arr| arr.each { |a| @answer6 += a + ' ' } }
+    p @answer6
+  end
+
+  def task7
+    sql = "SELECT COUNT(librarians.name)
+           FROM librarians
+           WHERE(librarians.id in
+           (SELECT orders.librarian_id
+           FROM orders INNER JOIN users on orders.user_id = users.id
+           WHERE users.name = '#{params[:name]}'
+           GROUP BY orders.librarian_id
+           HAVING COUNT(orders.user_id) >= 2))"
+    records_array = ActiveRecord::Base.connection.execute(sql).to_a
+    @answer7 = records_array.first.first
+    p @answer7
   end
 end
